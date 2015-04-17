5e633cdcc38956192ed4d0f30d14778eee30a9a6
Ics137 <Ics137@gmail.com>
2015-04-18 01:35:31 +0300

    first commit

---
+++ exercise_1/exercise_1/LinearFunction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    class LinearFunction
+    {
+
+        float a;
+        public float A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        float b;
+        public float B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public LinearFunction(float A, float B)
+        {
+        this.A = A;
+        this.B = B;
+        }
+
+        public float Functin(float X)
+        {
+            return A * X + B;
+        }    
+
+
+    }
+}
---
+++ exercise_1/exercise_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+
+        {
+
+
+            float A,B,X;
+          Console.Write("Введите переменную [a] : ");
+          A = float.Parse(Console.ReadLine());
+
+          Console.Write("Введите переменную [b] : ");
+          B = float.Parse(Console.ReadLine());
+
+          LinearFunction obj = new LinearFunction(A, B);
+
+          Console.Write("Введите переменную [x] : ");
+         X = float.Parse(Console.ReadLine());
+         Console.WriteLine("Значение функции   y= " + Math.Round(obj.A, 3) + "*" + Math.Round(X, 3) + "+" + Math.Round(obj.B,3) + " = " + Math.Round(obj.Functin(X), 3));
+
+         Console.ReadKey();
+        }
+    }
+}
