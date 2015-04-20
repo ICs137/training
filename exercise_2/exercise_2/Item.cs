7f09ee11e5bb475ff34ade8fe1e07b503ea41342
Ics137 <Ics137@gmail.com>
2015-04-20 12:31:40 +0300

    commit 2

---
+++ exercise_2/exercise_2/Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    class Item
+    {
+
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        Int32 price;
+        public Int32 Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        Int32 amt;
+        public Int32 Amt
+        {
+            get { return amt; }
+            set { amt = value; }
+        }
+
+
+        public Item(string nm, Int32 pr, Int32 am)
+        {
+            Name = nm;
+            Price = pr;
+            Amt = am;
+        }
+
+        public Int32 in_total()
+        {
+            return price * amt;
+        }
+
+    }
+}
---
+++ exercise_2/exercise_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+             ArrayList list = new ArrayList();
+             Random rnd= new Random();
+
+             for (Int32 i = 0; 10000 > i; i++) 
+                 {
+                 
+                      list.Add(  new Item ("name"+i, rnd.Next(1,1000),rnd.Next(1,500)));
+                 
+                 }
+
+             Int64 sum=0;
+             foreach (Item  i in list)
+                {
+                    
+                sum+=(i.in_total());
+                 
+                }
+
+             Console.WriteLine("Общая сумма всех стоимостей товаров ="+sum);
+
+        }
+    }
+}
