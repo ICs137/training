12a3007898a0f38569cc69a9fe8a617d3d5ae74a
Ics137 <Ics137@gmail.com>
2015-06-26 22:18:36 +0300

    Start

---
+++ Check Point4/PackerSalesReports/BL/FileReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BL
+{
+    public class FileReader
+    {
+        private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+        private readonly string _defaultFilePath;
+        private readonly string _defaultSaveFilePath;
+        private readonly string _fileName;
+        public string FileName
+        {
+            get { return _fileName; }
+        } 
+        public FileReader( FileSystemEventArgs args, string savePath)
+        {
+            _fileName = args.Name;
+            _defaultFilePath = args.FullPath;
+            _defaultSaveFilePath = savePath + args.Name;
+        }
+     
+        public List<string> GetContent()
+        {
+            return GetContent(_defaultFilePath, _defaultEncoding);
+        }
+        public List<string> GetContent(string filePath)
+        {
+            return GetContent(filePath, _defaultEncoding);
+        }
+        public List<string> GetContent(string filePath, Encoding encoding) //read file and separate the text into lines
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                using (StreamReader streamreader = new StreamReader(filePath, encoding))
+                {
+                    while (streamreader.Peek() >= 0)
+                    {
+                        lines.Add(streamreader.ReadLine().ToLower());
+                    }
+                }
+            }
+            catch (Exception except)
+            {
+                Console.WriteLine("The process failed: {0}", except.Message);
+            }
+            return lines;
+        }
+
+       
+
+
+
+    }
+}
---
+++ Check Point4/PackerSalesReports/DAL/Models/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public class Manager
+    {
+        public int ManagerId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
